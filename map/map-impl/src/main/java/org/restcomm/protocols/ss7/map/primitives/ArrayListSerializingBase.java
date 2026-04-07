fd3285ed7d22a56954849eb668cd6fd1f3489273
Huu Nhan Tran <nhanth87@gmail.com>
2026-04-07 22:59:56 +0700

    WIP: Continue MAP subscriberInformation Javolution to XStream migration
    Partial migration of remaining subscriberInformation files:
    - LocationInformationImpl.java (cleaned)
    
    Remaining: 9 subscriberInformation files + 26 TOOLS files

--- map/map-impl/src/main/java/org/restcomm/protocols/ss7/map/primitives/ArrayListSerializingBase.java
+++ map/map-impl/src/main/java/org/restcomm/protocols/ss7/map/primitives/ArrayListSerializingBase.java
@@ -32,35 +32,4 @@ public class ArrayListSerializingBase<T> {
         return this.data;
     }
 
-    /**
-     * XML Serialization/Deserialization
-     */
-    protected static final XMLFormat<ArrayListSerializingBase> ARRAY_LIST_SERIALIZING_BASE_XML = new XMLFormat<ArrayListSerializingBase>(
-            ArrayListSerializingBase.class) {
-
-        @Override
-        public void read(javolution.xml.XMLFormat.InputElement xml, ArrayListSerializingBase data) throws XMLStreamException {
-            data.data.clear();
-
-            while (xml.hasNext()) {
-                String localName = xml.getStreamReader().getLocalName().toString();
-                if (localName.equals(data.elementName)) {
-                    data.data.add(xml.get(data.elementName, data.classDef));
-                } else
-                    throw new XMLStreamException("Only <" + data.elementName + "> elements are allowed in this list. Found: " + localName);
-            }
-        }
-
-        @Override
-        public void write(ArrayListSerializingBase data, javolution.xml.XMLFormat.OutputElement xml) throws XMLStreamException {
-            if (data.data != null && data.data.size() > 0) {
-                for (int i1 = 0; i1 < data.data.size(); i1++) {
-                    Object pe = data.data.get(i1);
-                    if (pe != null)
-                        xml.add(pe, data.elementName, data.classDef);
-                }
-            }
-        }
-    };
-
 }
